33f2a73b3fcf7041c460fe0c2a78def937de5977
FrostDelta123 <pawel.soko2012@yandex.ru>
2019-04-29 21:47:01 +0300

    Config loader

--- src/main/java/ru/frostdelta/spongepowered/ForceReports.java
+++ src/main/java/ru/frostdelta/spongepowered/ForceReports.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.plugin.Plugin;
+import ru.frostdelta.spongepowered.config.ConfigManager;
+
+import java.io.IOException;
 
 @Plugin(
         id = "forcereports",
@@ -18,10 +21,19 @@ import org.spongepowered.api.plugin.Plugin;
 public class ForceReports {
 
     @Inject
-    private Logger logger;
+    private static Logger logger;
+
+    public static Logger getLogger() {
+        return logger;
+    }
 
     @Listener
     public void onServerStart(GameStartedServerEvent event) {
         logger.info("Developed by FrostDelta123");
+        try {
+            ConfigManager.loadConfig();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/ru/frostdelta/spongepowered/config/ConfigManager.java
@@ -0,0 +1,43 @@
+package ru.frostdelta.spongepowered.config;
+
+import com.google.inject.Inject;
+import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.config.ConfigDir;
+import org.spongepowered.api.config.DefaultConfig;
+import ru.frostdelta.spongepowered.ForceReports;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+
+public class ConfigManager {
+
+    @Inject
+    @DefaultConfig(sharedRoot = true)
+    private Path defaultConfig;
+
+    @Inject
+    @DefaultConfig(sharedRoot = true)
+    private static ConfigurationLoader<CommentedConfigurationNode> loader;
+
+    @Inject
+    @ConfigDir(sharedRoot = false)
+    private Path privateConfigDir;
+
+
+    public static void loadConfig() throws IOException {
+        ConfigurationNode rootNode;
+        rootNode = loader.load();
+        if (!rootNode.hasMapChildren()) { // is empty
+            ForceReports.getLogger().info("No config found - loading default");
+            URL jarConfigFile = Sponge.getAssetManager().getAsset("config.conf").get().getUrl();
+            ConfigurationLoader<CommentedConfigurationNode> defaultLoader =
+                    HoconConfigurationLoader.builder().setURL(jarConfigFile).build();
+            rootNode = defaultLoader.load();
+        }
+    }
+}
